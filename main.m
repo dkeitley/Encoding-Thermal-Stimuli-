3e194219b2c027cb38b7d9ea3b3c84bdb5056d45
Daniel Keitley <dkeitley@gmail.com>
2016-08-02 12:01:55 +0100

    Added more data and resting rate.

--- main.m
+++ main.m
@@ -1,34 +1,36 @@
 
 % COMMENTS
-% Min found : 124.49
-% Resting rate calculated as an average of spontaneous rates
+% Min found : 122.8290
+% theta: 0.4843 -6.1052 8.7438 0.0255 0 *10^3
+% Look at similarlities between cells. Are they grouped?
+% Normalise the parameters - does the derivative have much effect? 
   
 [spikes_A,temps_A,resting_A] = getData('/home/daniel/Encoding Thermal Stimuli/A fibre ramp positions.xlsx');
 [spikes_C,temps_C,resting_C] = getData('/home/daniel/Encoding Thermal Stimuli/C fibre ramp positions.xlsx');
 
 disp('Data Loaded');
 
-% t = constructTraining(spikes_C,temps_C,resting_C,0.25);
-% y = t(:,4);
-% X = t(:,2:3);
-% r0 = t(:,1);
-% 
-% %theta - a, b, x0, L, k
-% opt_theta = [0;0;0;0;0];
-% opt_cost = Inf;
-% 
-% for i = 1:100
-%     initial = [-100 + rand*200,-100 + rand*200,-200 + rand*400, rand*100,-100 + rand*200]';
-%     [theta,cost] = fminsearch(@(t)(costFunction(t,X,y,r0)),initial,optimset('MaxFunEvals',5000,'MaxIter',5000));
-% 
-%     if(cost < opt_cost)
-%         opt_cost = cost;
-%         opt_theta = theta;
-%     end
-%     
-% end
-% 
-% disp('Theta found');
+t = constructTraining(spikes_A,temps_A,resting_A,0.25);
+y = t(:,4);
+X = t(:,2:3);
+r0 = t(:,1);
+
+%theta - a, b, x0, L, k
+opt_theta = [0;0;0;0;0];
+opt_cost = Inf;
+
+for i = 1:100
+    initial = [-100 + rand*200,-100 + rand*200,-200 + rand*400, rand*100,-100 + rand*200]';
+    [theta,cost] = fminsearch(@(t)(costFunction(t,X,y,r0)),initial,optimset('MaxFunEvals',5000,'MaxIter',5000));
+
+    if(cost < opt_cost)
+        opt_cost = cost;
+        opt_theta = theta;
+    end
+    
+end
+
+disp('Theta found');
 
 
 %[a,b] = maximumLikelihood(X(:,1),y,X(:,2));
