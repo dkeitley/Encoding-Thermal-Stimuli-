2fbc5869caceb287c304e3d43745a4d3d2e83ed7
Daniel Keitley <dkeitley@gmail.com>
2016-08-03 13:16:07 +0100

    Added mean normalisation to features.

--- constructTraining.m
+++ constructTraining.m
@@ -8,7 +8,9 @@ function training = constructTraining(spikes,temps,resting,binSize)
         [p,dp,~] = getPoly(temps{i},5);
         [rate,tvec] = getRate(spikes{i},binSize);
         temp = polyval(p,tvec);
+        temp = (temp - mean(temp))/(max(temp)-min(temp));
         dtemp = polyval(dp,tvec);
+        dtemp = (dtemp - mean(dtemp))/(max(dtemp)-min(dtemp));
         
         training = [training;[ones(size(tvec))*resting{i},temp,dtemp,rate]];
     end
