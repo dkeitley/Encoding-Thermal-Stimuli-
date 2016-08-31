f5422bc6898220d8e16e4ce32870076a45ff5097
Daniel Keitley <dkeitley@gmail.com>
2016-08-31 18:01:17 +0100

    Updated ploTuningCurve to instead plot un-averaged firing rates as a function of temperature.

---
+++ plotTempResponse.m
@@ -0,0 +1,44 @@
+function [tvec,rate] = plotTempResponse(spike,temp)
+
+    temps = {};
+    spikes = {};
+    if((iscell(spike) && iscell(temp)))
+        temps = temp;
+        spikes = spike;
+    else
+        temps{1} = temp;
+        spikes{1} = spike;
+    end
+
+    numTrials = size(spikes',1);
+    m = ceil(numTrials/6);
+    figure()
+    
+     for trial = 1:numTrials
+         %find spikes times within 1 degree temp range
+         [p,dp,tvec] = getPoly(temps{trial},5);
+         temppoly = polyval(p,tvec);
+         dtemppoly = polyval(dp,tvec);
+         rate = [];
+       
+         for t = 20:60
+             window = find((t-0.5 < temppoly) & (t + 0.5 >= temppoly));
+             numSpikes = sum(spikes{trial}(window));
+             duration = (max(window) - min(window))*(5*10^-5);
+             if(isempty(duration) == 1)
+                rate(t-19) = 0;
+             else
+                rate(t-19) = numSpikes/duration;
+             end
+         end
+         if(numTrials > 1)
+             subplot(m,6,trial)
+         end
+         range = find(rate ~=0);
+         rate = rate(1:range(end));
+         tvec = [20:size(rate,2)+19]';
+         plot(tvec,rate);
+         title(strcat('Trial',int2str(trial)));
+
+     end
+end
